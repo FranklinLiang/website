f45b39cb24ef625a77f717e5019b304b27ffff74
Franklin Liang <franklin.liang@uwaterloo.ca>
2024-12-18 02:20:15 -0500

    Added nmap and ftp functionality. Fixed up some small bugs

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -3,6 +3,8 @@
 import * as bin from './index';
 import config from '../../../config.json';
 import files from '../../../files.json';
+import adversary from '../../../adversary.json'
+import network from '../../../network.json'
 
 // Help
 export const help = async (args: string[]): Promise<string> => {
@@ -134,7 +136,7 @@ export const cat = async (args: string[]): Promise<string> => {
   };
 };
 
-//tcpdump command looks at (simulated) network traffic
+//ifconfig command looks at (fake) network information
 export const ifconfig = async (args: string[]): Promise<string> => {
   return `eth0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
         inet 192.168.1.1  netmask 255.255.240.0  broadcast 192.168.1.255
@@ -157,17 +159,17 @@ lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
 
 //tcpdump command looks at (simulated) network traffic
 export const tcpdump = async (args: string[]): Promise<string> => {
-
+  //grabbing current user time
   const currentTime = new Date();
   const hours = String(currentTime.getHours()).padStart(2, '0');
   const minutes = String(currentTime.getMinutes()).padStart(2, '0');
   const formattedTime = `${hours}:${minutes}`;
 
-  return `${formattedTime}:32.123456 IP 192.168.1.10.54872 > 192.168.1.1.80: Flags [S], seq 1234567890, win 65535, options [mss 1460,sackOK,TS val 123456789 ecr 0,nop,wscale 7], length 0
-${formattedTime}:32.123789 IP 192.168.1.1.80 > 192.168.1.10.54872: Flags [S.], seq 987654321, ack 1234567891, win 65535, options [mss 1460,sackOK,TS val 987654321 ecr 123456789,nop,wscale 7], length 0
-${formattedTime}:32.124012 IP 192.168.1.10.54872 > 192.168.1.1.80: Flags [.], ack 987654322, win 65535, options [nop,nop,TS val 123456790 ecr 987654321], length 0
-${formattedTime}:32.124345 IP 192.168.1.10.54872 > 192.168.1.1.80: Flags [P.], seq 1234567891:1234567903, ack 987654322, win 65535, options [nop,nop,TS val 123456791 ecr 987654321], length 12
-${formattedTime}:32.124678 IP 192.168.1.1.80 > 192.168.1.10.54872: Flags [.], ack 1234567903, win 65535, options [nop,nop,TS val 987654322 ecr 123456791], length 0
+  return `${formattedTime}:32.123456 IP 192.168.2.17.54872 > 192.168.1.1.80: Flags [S], seq 1234567890, win 65535, options [mss 1460,sackOK,TS val 123456789 ecr 0,nop,wscale 7], length 0
+${formattedTime}:32.123789 IP 192.168.1.1.80 > 192.168.2.17.54872: Flags [S.], seq 987654321, ack 1234567891, win 65535, options [mss 1460,sackOK,TS val 987654321 ecr 123456789,nop,wscale 7], length 0
+${formattedTime}:32.124012 IP 192.168.2.17.54872 > 192.168.1.1.80: Flags [.], ack 987654322, win 65535, options [nop,nop,TS val 123456790 ecr 987654321], length 0
+${formattedTime}:32.124345 IP 192.168.2.17.54872 > 192.168.1.1.80: Flags [P.], seq 1234567891:1234567903, ack 987654322, win 65535, options [nop,nop,TS val 123456791 ecr 987654321], length 12
+${formattedTime}:32.124678 IP 192.168.1.1.80 > 192.168.2.17.54872: Flags [.], ack 1234567903, win 65535, options [nop,nop,TS val 987654322 ecr 123456791], length 0
 ${formattedTime}:39.543210 IP 203.0.113.5.56789 > 192.168.1.1.21: Flags [S], seq 654321098, win 65535, options [mss 1460,sackOK,TS val 223344556 ecr 0,nop,wscale 7], length 0
 ${formattedTime}:39.543789 IP 192.168.1.1.21 > 203.0.113.5.56789: Flags [S.], seq 789012345, ack 654321099, win 65535, options [mss 1460,sackOK,TS val 556677889 ecr 223344556,nop,wscale 7], length 0
 ${formattedTime}:39.544012 IP 203.0.113.5.56789 > 192.168.1.1.21: Flags [.], ack 789012346, win 65535, options [nop,nop,TS val 223344557 ecr 556677889], length 0
@@ -175,6 +177,166 @@ ${formattedTime}:39.544345 IP 203.0.113.5.56789 > 192.168.1.1.21: Flags [P.], se
 ${formattedTime}:39.544678 IP 192.168.1.1.21 > 203.0.113.5.56789: Flags [F.], seq 789012346, ack 654321121, win 65535, options [nop,nop,TS val 556677890 ecr 223344558], length 0
 ${formattedTime}:39.545012 IP 203.0.113.5.56789 > 192.168.1.1.21: Flags [R], seq 654321121, win 0, length 0
 `;
+};
+
+//nmap command scans (fake) ips to see running services, os information, etc...
+export const nmap = async (args: string[]): Promise<string> => {
+  /*
+  two IPs will return unique data:
+  203.0.113.5 will be the external adversary IP
+  192.168.2.17 is another machine on the network
+  all other IPs will fail to resolve
+  */
+
+  const isValidIPv4 = (ip: string): boolean => {
+    const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
+    return ipv4Regex.test(ip);
+  };
+
+  //Getting the date and time in UTC for the nmap command outputs
+  const getCurrentDateTimeUTC = (): string => {
+    const now = new Date();
+
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(now.getUTCDate()).padStart(2, '0');
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes} UTC`;
+  };
+
+  if (args[0] === undefined) {
+    //&lt; is <
+    //&gt; is >
+    return `use format: nmap &lt;ipv4_address&gt;`;
+  }
+
+  else if (args[0] === "203.0.113.5") { //adversary IP
+    return `Starting Nmap 7.93 ( https://nmap.org ) at ${getCurrentDateTimeUTC()}
+Nmap scan report for 203.0.113.5
+Host is up (0.015s latency).
+Not shown: 997 closed ports
+PORT     STATE SERVICE
+21/tcp   open  ftp
+80/tcp   open  http
+443/tcp  open  https
+
+Nmap done: 1 IP address (1 host up) scanned in 0.52 seconds`
+  } else if (args[0] === "192.168.2.17") { //internal network IP
+    return `Starting Nmap 7.93 ( https://nmap.org ) at ${getCurrentDateTimeUTC()}
+Nmap scan report for 192.168.2.17
+Host is up (0.005s latency).
+Not shown: 998 closed ports
+PORT     STATE SERVICE
+22/tcp   open  ssh
+3389/tcp open  ms-wbt-server
+
+Nmap done: 1 IP address (1 host up) scanned in 0.37 seconds`
+  } else if (isValidIPv4(args[0])) { //all other valid IP
+    return `Starting Nmap 7.93 ( https://nmap.org ) at ${getCurrentDateTimeUTC()}
+Failed to resolve "${args[0]}".
+Nmap done: 0 IP addresses (0 hosts up) scanned in 0.02 seconds`
+  } else { //invalid IP
+    return `Starting Nmap 7.93 ( https://nmap.org ) at ${getCurrentDateTimeUTC()}
+Invalid IPv4 address "${args[0]}".
+Nmap done: 0 IP addresses (0 hosts up) scanned in 0.00 seconds`
+  }
+};
+
+export const ftp = async (args: string[]): Promise<string> => {
+  if (args[0] === undefined) {
+    return `use the flag --help to see a list of commands`
+  } else if (args[0] === "--help") {
+    //&lt; is <
+    //&gt; is >
+    return `ftp &lt;ipv4_address&gt; &lt;flags&gt;
+FLAGS
+  -p <password>: Password
+  -l: List files/directories
+  -v <file_name>: View file content`
+  }
+
+  //parse flags and store flag values
+  let ip : string | null = null;
+  let password : string | null = null;
+  let file_name : string | null = null;
+  let list : boolean | null = false;
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+
+    if (!ip && !arg.startsWith('-')) {
+      ip = arg;
+    }
+  
+    else if (arg === '-p' && i+1 < args.length) {
+      password = args[i+1];
+      i++;
+    }
+
+    else if (arg === '-v' && i+1 < args.length) {
+      file_name = args[i+1];
+      i++;
+    }
+
+    else if (arg === '-l') {
+      list = true;
+    }
+  }
+
+  const isValidIPv4 = (ip: string): boolean => {
+    const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
+    return ipv4Regex.test(ip);
+  };
+  
+  //Main handling logic, checks if ip provided is special ip, otherwise provide error output
+  if (ip === "203.0.113.5" || ip === "192.168.2.17") {
+    let machine;
+
+    if (ip === "203.0.113.5") {
+      machine = adversary;
+
+      if (password !== Buffer.from("aHVudGVyMg==", 'base64').toString('utf-8')) {
+        return `ftp: connect: Incorrect password`;
+      }
+
+    } else if (ip === "192.168.2.17") {
+      machine = network;
+
+      if (password !== Buffer.from("YnVubnk=", 'base64').toString('utf-8')) {
+        return `ftp: connect: Incorrect password`;
+      }
+    }
+    //list files command
+    if (list) {
+      let file_names = "";
+  
+      machine.files.forEach(file => {
+      file_names += `${file.name}\n`;
+    });
+  
+    return file_names;
+    }
+    //display file content command
+    if (file_name) {
+      const file = machine.files.find(file => file.name === file_name);
+
+      if (file) {
+        return `${file.content}`;
+      } else {
+        return `File ${file_name} does not exist.`;
+      };
+    }
+  }
+
+  else if (isValidIPv4(ip)) {
+    return `ftp: connect: Connection timed out`;
+  }
+
+  else {
+    return `ftp: connect: Invalid IPv4 address`;
+  }
 };
 
 export const cd = async (args: string[]): Promise<string> => {
