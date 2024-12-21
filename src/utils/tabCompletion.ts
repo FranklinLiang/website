e96f80dbb855cf148a519ae1c17cb6cab829aaee
Franklin Liang <franklin.liang@uwaterloo.ca>
2024-12-21 03:09:30 -0500

    Added tab completion for file names for the cat function

--- src/utils/tabCompletion.ts
+++ src/utils/tabCompletion.ts
@@ -1,14 +1,31 @@
 import * as bin from './bin';
+import files from '../../files.json';
 
 export const handleTabCompletion = (
   command: string,
   setCommand: React.Dispatch<React.SetStateAction<string>>,
 ) => {
-  const commands = Object.keys(bin).filter((entry) =>
-    entry.startsWith(command),
-  );
+  // Split command into parts
+  const parts = command.split(' ');
 
-  if (commands.length === 1) {
-    setCommand(commands[0]);
+  // If it's a single word, complete commands
+  if (parts.length === 1) {
+    const commands = Object.keys(bin).filter((entry) =>
+      entry.startsWith(command),
+    );
+
+    if (commands.length === 1) {
+      setCommand(commands[0]);
+    }
+  }
+  // If it starts with "cat" and has a second part, complete filenames
+  else if (parts[0] === 'cat' && parts.length === 2) {
+    const fileNames = files.files
+      .map((file) => file.name)
+      .filter((name) => name.startsWith(parts[1]));
+
+    if (fileNames.length === 1) {
+      setCommand(`cat ${fileNames[0]}`);
+    }
   }
 };
